0c14a0eac4fa091fb17da6d30e1d921835ebcc26
MATHEADS\urajkovic <uros.rajkovic@mathema.de>
2022-10-25 23:42:13 +0200

    new editor and nav hover list item and index.php remove from url

--- administrator/cache/_media_version/13845065424eb2b3f7fb36b9f8bdf288-cache-_media_version-96b33e647d8feca69268b8c1c1323b59.php
+++ administrator/cache/_media_version/13845065424eb2b3f7fb36b9f8bdf288-cache-_media_version-96b33e647d8feca69268b8c1c1323b59.php
@@ -1,1 +1,1 @@
-<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:32:"ea4f07f912980f158624853378b85e4f";s:6:"output";s:0:"";}
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:32:"47b77e8351ee45a22ebe8494c67b79b4";s:6:"output";s:0:"";}
