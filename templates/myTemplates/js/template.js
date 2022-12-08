6eb47759bb7e4bfb709c56243a9b4a542c27961c
MATHEADS\urajkovic <uros.rajkovic@mathema.de>
2022-12-08 20:38:40 +0100

    content from index replace to admin panel.  new database and jquery

--- templates/myTemplates/js/template.js
+++ templates/myTemplates/js/template.js
@@ -11,13 +11,41 @@ $(".read-more-portfolio").click(function () {
 
 });
 $(".edu-content").on("mouseover", function () {
-    $(this).children('p').css("display","none");
-    $(this).children('img').css("visibility","hidden");
+    $(this).children('p').css("display", "none");
+    $(this).children('img').css("visibility", "hidden");
     $(this).children().addClass("hover-text-style");
 });
 
 $(".edu-content").mouseleave(function () {
-    $(this).children('p').css("display","block");
-    $(this).children('img').css("visibility","visible");
+    $(this).children('p').css("display", "block");
+    $(this).children('img').css("visibility", "visible");
     $(this).children().removeClass("hover-text-style");
+});
+/* Slider */
+$(".click-icon").hover(function () {
+    $(this).css('cursor', 'pointer');
+
+});
+$(".plus-minus").click(function () {
+    let parentClass = $(this).parent().parent().parent().get(0);
+    let src = $(this).attr('src').split('/');
+    let file = src[src.length - 1];
+    $(this).attr('src');
+    if (file == 'plus.png') {
+        $(parentClass).find(".hidden-block").slideDown();
+        let src = $(this).attr('src').replace("plus.png", "minus.png");
+        $(this).attr('src', src);
+        $(this).attr('src', src).css({
+            'filter': 'invert(11%) sepia(81%) saturate(6280%) hue-rotate(348deg) brightness(100%) contrast(89%)'
+        });
+    }
+    if (file == 'minus.png') {
+        $(parentClass).find(".hidden-block").slideUp();
+        let src = $(this).attr('src').replace("minus.png", "plus.png");
+        $(this).attr('src', src);
+        $(this).attr('src', src).css({
+            'filter': 'invert(0%) sepia(4%) saturate(16%) hue-rotate(279deg) brightness(109%) contrast(101%)'
+        });
+    }
+
 });
